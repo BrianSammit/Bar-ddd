62d059de7bb13ad3f63abaefbcbcd5a5cd52f4e4
BrianSammit <briansammit.cruz@gmail.com>
2023-03-08 23:15:18 -0500

    Add order event change

--- src/main/java/com/brian/bar/domain/order/OrderEventChange.java
+++ src/main/java/com/brian/bar/domain/order/OrderEventChange.java
@@ -1,5 +1,8 @@
 package com.brian.bar.domain.order;
 
+import com.brian.bar.domain.drink.Drink;
+import com.brian.bar.domain.drink.values.DrinkID;
+import com.brian.bar.domain.order.event.DrinkAdded;
 import com.brian.bar.domain.order.event.OrderCreated;
 import com.brian.bar.domain.order.values.Modification;
 import com.brian.bar.domain.order.values.Status;
@@ -15,6 +18,13 @@ public class OrderEventChange extends EventChange {
             order.modification = new Modification(event.getModification());
             order.tableID = TableID.of(event.getTableID());
             order.drinks = new ArrayList<>();
+        });
+
+        apply((DrinkAdded event) -> {
+            Drink drink = new Drink(
+                    DrinkID.of(event.getDrinkID())
+            );
+            order.drinks.add(drink);
         });
     }
 }
