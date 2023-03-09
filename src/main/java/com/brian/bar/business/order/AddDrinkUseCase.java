3dc12c76848852c45ad6934ad6f64e7fe07d98f6
BrianSammit <briansammit.cruz@gmail.com>
2023-03-09 11:23:36 -0500

    Add drink use case

---
+++ src/main/java/com/brian/bar/business/order/AddDrinkUseCase.java
@@ -0,0 +1,30 @@
+package com.brian.bar.business.order;
+
+import com.brian.bar.business.commons.EventRepository;
+import com.brian.bar.business.commons.UseCaseForCommand;
+import com.brian.bar.domain.drink.values.DrinkID;
+import com.brian.bar.domain.order.Order;
+import com.brian.bar.domain.order.command.AddDrinkCommand;
+import com.brian.bar.domain.order.values.OrderID;
+import com.brian.bar.generic.DomainEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class AddDrinkUseCase implements UseCaseForCommand<AddDrinkCommand> {
+
+    private final EventRepository eventRepository;
+
+    public AddDrinkUseCase(EventRepository eventRepository){
+        this.eventRepository = eventRepository;
+    }
+    @Override
+    public List<DomainEvent> apply(AddDrinkCommand command) {
+        List<DomainEvent> orderEvents = eventRepository.findByAggregatedRootId(command.getOrderID());
+        Order order = Order.from(OrderID.of(command.getOrderID()), orderEvents);
+        order.addDrink(DrinkID.of(command.getDrinkID()), command.getName(), command.getPrice(), command.getModification());
+        return order.getUncommittedChanges().stream().map(eventRepository::saveEvent).collect(Collectors.toList());
+    }
+}
