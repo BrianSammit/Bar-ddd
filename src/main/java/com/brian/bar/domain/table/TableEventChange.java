cdcaf001217b1d5f3f5f900f6c8e7e9d970d1817
BrianSammit <briansammit.cruz@gmail.com>
2023-03-09 16:13:25 -0500

    Add table event change

--- src/main/java/com/brian/bar/domain/table/TableEventChange.java
+++ src/main/java/com/brian/bar/domain/table/TableEventChange.java
@@ -1,4 +1,19 @@
 package com.brian.bar.domain.table;
 
-public class TableEventChange {
+import com.brian.bar.domain.table.event.TableCreated;
+import com.brian.bar.domain.table.values.TableNum;
+import com.brian.bar.domain.table.values.Waiter;
+import com.brian.bar.generic.EventChange;
+
+import java.util.ArrayList;
+
+public class TableEventChange extends EventChange {
+
+    public TableEventChange(Table table) {
+        apply((TableCreated event) -> {
+            table.tableNum = new TableNum(event.getTableNum());
+            table.waiter = new Waiter(event.getWaiter());
+            table.costumers = new ArrayList<>();
+        } );
+    }
 }
