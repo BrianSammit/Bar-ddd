7f782c2f67fca5125be8754a28fbed591d27b671
BrianSammit <briansammit.cruz@gmail.com>
2023-03-09 20:37:25 -0500

    Add waiter entity

---
+++ src/main/java/com/brian/bar/domain/table/Waiter.java
@@ -0,0 +1,18 @@
+package com.brian.bar.domain.table;
+
+import com.brian.bar.domain.drink.values.Name;
+import com.brian.bar.domain.table.values.CostumerID;
+import com.brian.bar.domain.table.values.WaiterID;
+import com.brian.bar.generic.Entity;
+
+public class Waiter extends Entity<WaiterID> {
+    private Name name;
+    public Waiter(WaiterID id, Name name) {
+        super(id);
+        this.name = name;
+    }
+
+    public Name getName() {
+        return name;
+    }
+}
